2d3b9d07a1413a506360804e2d67948aaf1d2b79
sevda <imanysevda@gmail.com>
2020-08-22 09:04:07 +0430

    add scrolling to element

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    /* for the sticky navigation */
 $(".js--section-features").waypoint(function(direction){
 
     if (direction == "down"){
@@ -7,13 +8,34 @@ $(".js--section-features").waypoint(function(direction){
     }
     else {
         $('nav').removeClass("sticky");
-
-    }
-
-},
-{
-    offset:"60px ;"
+}},
+{  offset:"60px ;"
 });
 
 
+    /* scroll on buttons */
+    $('.js--scroll-to-products').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-products').offset().top},1000);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+    });
+
+    /* navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+  
 });
